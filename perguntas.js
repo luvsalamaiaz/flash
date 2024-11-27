6d57107e919ed810a62ae5e9b9288d63624ddacd
Vitória Barboza Malafaia <vitoriamalafaia.220107@gmail.com>
2024-11-27 09:43:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao (
+    'História',
+    'Em qual período da pré-história o fogo foi descoberto?',
+    'O fogo foi descoberto no período Paleolítico'
+)
+
+criaCartao (
+    'História',
+    'Qual o tema do famoso discurso "Eu Tenho um Sonho" de Martin Luther King?',
+    'Igualdade racial'
+)
+
+criaCartao (
+    'História',
+    'Qual foi o primeiro animal, sem ser o ser humano, a sair da Terra?',
+    'Foi uma cadela, Laika, enviada pelos russos na Guerra Fria'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual é o único continente com terra nos quatro hemisférios?',
+    'O continente Africano'
+)
+
+criaCartao (
+    'Geografia',
+    'Verdadeiro ou falso: A Holanda é uma região dos Países Baixos?',
+    'Verdadeiro'
+)
+
+criaCartao (
+    'Geografia',
+    'Quais são as cores da bandeira da Organização das Nações Unidas (ONU)?',
+    'Azul e Branco'
+)
+
+criaCartao (
+    'Matemática',
+    'O Planeta Terra possui um raio de cerca de 6 371 km. Se considerarmos a Terra como uma esfera perfeita, qual o comprimento de sua circunferência?',
+    'O comprimento de uma circunferência é calculado pela relação:C = 2. pi. r   Substituindo os valores, temos:    C = 2 . 3,14 . 6.371 km   C = 40.009,88 km'
+)
+
+criaCartao (
+    'Matemática',
+    'Quantos pontos podemos encontrar em um par de dados?',
+    '42 pontos (1+2+3+4+5+6 = 21 . 2 = 42)'
+)
+
+criaCartao (
+    'Matemática',
+    'Sorteando-se um número de 1 a 20, qual a probabilidade de que esse número seja múltiplo de 2?',
+    'A quantidade de números múltiplos de dois são: A = (2, 4, 6, 8, 10, 12, 14, 16, 18, 20)             Substituindo os valores na fórmula de probabilidade, temos: P = número de casos que nos interessam / número total de casos possíveis  P = 10 / 20 = 1 / 2 = 0,5 (ou 50%)'
+)
